c538e233ab397e12155183e3808414fd65daec9e
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-01-29 14:17:58 +0200

    Add files via upload

---
+++ ft_list_size.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 13:26:55 by iniska            #+#    #+#             */
+/*   Updated: 2024/01/29 14:06:06 by iniska           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	i;
+
+	i = 0;
+	while (begin_list)
+	{
+		i++;
+        begin_list = begin_list->next;
+	}
+    return (i);
+}
+
