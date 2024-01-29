e21a032d2be4925ec38fe6120c4b0e677438a7ff
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-01-29 09:55:47 +0200

    Add files via upload

--- rev_print.c
+++ rev_print.c
@@ -5,33 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/04 14:33:10 by iniska            #+#    #+#             */
-/*   Updated: 2024/01/04 15:33:22 by iniska           ###   ########.fr       */
+/*   Created: 2024/01/29 09:00:59 by iniska            #+#    #+#             */
+/*   Updated: 2024/01/29 09:54:36 by iniska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	rev_print(char *str)
+void	revprint(char *av)
 {
-	int		i;
+	int i;
 
-	if (!str)
-		return ;
 	i = 0;
-	while (str[i] != '\0')
+	while (av[i] != '\0')
 		i++;
 	while (i >= 0)
 	{
-		write(1, &str[i], 1);
+		write(1, &av[i], 1);
 		i--;
 	}
-	write(1, "\n", 1);
 }
 
-int main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-	if (argc == 2)
-			rev_print(argv[1]);	
-	return(0);
+	if (ac == 2)
+		revprint(av[1]);
+	write(1, "\n", 1);
+	return (0);
 }
+
