8c6128f1e0bd127129454ba1ddb2c41c597a6d45
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-06-05 17:03:05 +0300

    Update fizzbuzz.c
    this one is so much better!

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -11,44 +11,33 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
-void	fizzbuzz(int i)
-{		
-	char	*nmb;
-	int		len;
-
-	len = 0;
-	if (i >= 1 && i <= 100)
-	{
-		if (i % 3 != 0 && i % 5 != 0)
-		{
-			nmb = (char *)malloc(12);
-			if (!nmb)
-				return ;
-			while (i > 0)
-			{
-				nmb[len++] = i % 10 + '0';
-				i /= 10;
-			}
-			while (len > 0)
-				write(1, &nmb[--len], 1);
-			write(1, "\n", 1);
-			free(nmb);
-			return ;
-		}
-		if (i % 3 == 0 && i % 5 == 0)
-			write (1, "fizzbuzz\n", 9);
-		if (i % 3 == 0)
-			write(1, "fizz\n", 4);
-		if (i % 5 == 0)
-			write (1, "buzz\n", 4);
-	}
-}
-
-int main()
+void	nmb(int i)
 {
-	int nmb = 98;
-	fizzbuzz(nmb);
+	char str[10] = "0123456789";
+
+	if (i > 9)
+			nmb(i / 10);
+	write (1, &str[i % 10], 1);
+}	
+
+int	main()
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write (1, "buzz", 4);
+		else
+			nmb(i);
+		write(1, "\n", 1);
+		i++;
+	}
 	return (0);
 }
