70d184ae2fe9f82a0871ec2af438a940b6df7986
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-08-05 11:31:08 +0300

    Add files via upload

--- paramsum.c
+++ paramsum.c
@@ -1,31 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   paramsum.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/12 14:48:30 by iniska            #+#    #+#             */
-/*   Updated: 2024/01/12 15:12:19 by iniska           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	ft_putnum(int n)
+void	writenmb(int n)
 {
-	char num;
+	char	num;
 
 	if (n >= 10)
-		ft_putnum(n / 10);
+		writenmb(n / 10);
 	num = (n % 10) + '0';
 	write(1, &num, 1);
 }
 
-int	main(int ac, char **av)
-{
 
-	ft_putnum(ac - 1);
-	write (1, "\n", 1);
+
+
+int main(int arc, char **arv)
+{
+	
+	(void)arv;
+
+	if(arc == 1)
+		write(1, "0", 1);
+	else if (arc >= 2)
+		writenmb(arc - 1);
+	write(1, "\n", 1);
 	return (0);
 }
+			
